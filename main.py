05c877d12da8532daba8a1d99524f3b9d3dcbea0
Aryan Nagpal <arcnagpal1234@gmail.com>
2025-07-15 23:20:57 -0400

    FireRemote v0.0.2: UI updates, backend accepts ip.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect
+import logging
+import subprocess
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
 KEYS = {
     "up": 19,
@@ -15,13 +19,26 @@ KEYS = {
     "play_pause": 85
 }
 
+def send_key(keycode, FIRESTICK_IP):
+    subprocess.run(["adb", "connect", FIRESTICK_IP], capture_output=True)
+    subprocess.run(["adb", "shell", "input", "keyevent", str(keycode)], capture_output=True)
+
+
 @app.route('/', methods = ["GET", "POST"])
 def index():
     if request.method == "POST":
         button = request.form.get("button")
+        #TODO Clean the IP, raise errors if bad
+        FIRESTICK_IP = request.form["ip"]    
         if button in KEYS:
-            print("yay")
+            try:
+                send_key(KEYS[button], FIRESTICK_IP)
+            except Exception as e:
+                logger.error(f"{e}: Not able to communicate message to Fire TV")
+
         return redirect('/')
     return render_template("remote.html")
 
-app.run()
+
+if __name__ == "__main__":
+    app.run()
